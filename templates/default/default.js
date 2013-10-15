7cddfe7befaf99fe360233d18e0e978e4ce81861
Juan Davila <juan23davila@gmail.com>
2013-10-15 11:34:00 -0500

    Ya funciona jQuery

--- templates/default/default.js
+++ templates/default/default.js
@@ -1,6 +1,9 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+$(document).on('ready', function(){
+    
+    $('#menu2').click(function(){
+        alert("sirvio");
+    });
+    
+});
 
 
