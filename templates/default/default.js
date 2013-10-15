6cfa10e49437342fb3c346aa3f7a26cd6712c008
Juan Davila <juan23davila@gmail.com>
2013-10-15 13:02:57 -0500

    agregue una funcion jQuery

--- templates/default/default.js
+++ templates/default/default.js
@@ -1,9 +1,21 @@
-$(document).on('ready', function(){
-    
-    $('#menu2').click(function(){
+$(document).on('ready', function() {
+
+    $('#menu2').click(function() {
         alert("sirvio");
     });
-    
+
+});
+
+
+$(document).ready(function() {
+
+    $('#menu2:has(ul)').click(
+            function(e)
+            {
+                $(this).find('ul').css({display: "block"});
+            }
+    );
+
 });
 
 
