7120b4cc47c07e22e88e781ea51065446decb881
anfho93 <anfho93@gmail.com>
2013-10-25 20:45:15 -0500

    Acceso a las parabolicas y cargado de los horarios de a 2 horas

--- modulos/rovi/views/Grid.php
+++ modulos/rovi/views/Grid.php
@@ -10,51 +10,88 @@
  *
  * @author Andres
  */include_once './lib/ViewController.php';
-class GridViewController extends ViewController{
-    
+
+class GridViewController extends ViewController {
+
     /**
      * 
      * @param type $inicio, Horario de inicio del Horario
      */
-    public function getHTML($inicio=6) {
+    public function getHTML($inicio = 6) {
         //se crea el header.
-        $headergrid = '<div class="grid_header" style=" position: relative;">
-
-            
-            <nav>
-                <ul>
-                    <li>
-                        <div class="prev">
-
-                            Canales
-
-                        </div>
-                    </li>
-                    <li>
-                        <div class="prev">
-
-                            Prev
-
-                        </div>
-                    </li>
-                    <li>6:00</li>
-                    <li>7:00</li>
-                    <li>8:00</li>                    
-                    <li>9:00</li>
-                    <li><div class="next">
-
-                            Next
-
-                        </div></li>
-                </ul>
-            </nav>
-
-            
-
-        </div>';
-        return $headergrid;
-        
+        return $this->getHeaderGrid($inicio);
     }
-}
 
+    private function getHeaderGrid($inicio) {
+        $headergrid = '<div>
+
+            
+            <nav class="menucabecera">
+                <ul >
+                    <li class="canales"><div>Canales</div></li>
+                    <li class="prev"><                 </li>
+                   ';
+        for ($index = $inicio; $index < ($inicio + 3); $index++) {
+
+            $headergrid.='<li  class="hora">' . ($index % 24) . ":00</li>";
+            $headergrid.='<li  class="hora">' . ($index % 24) . ":30</li>";
+        }
+
+        $headergrid.='<li class="prev">
+                                        >
+                                      </li>
+                                      </ul>
+            </nav>
+        </div>';
+        return $headergrid.=$this->getCSS();
+    }
+
+    public function getCSS() {
+        return "
+            <style>
+           .menucabecera ul{
+                padding: 0;
+                background: rgba(0,0,0,.5);
+                font-size: 0;
+            }
+             .canales {
+                display: inline-block;               
+                font-size: 14.4px;
+                box-sizing: border-box;
+                -moz-box-sizing: border-box;
+                -webkit-box-sizing: border-box;
+                text-align: center;
+                 width: 8%;
+            }
+
+            .hora{
+                display: inline-block;
+                width: 14.28%;
+                font-size: 14.4px;
+                box-sizing: border-box;
+                -moz-box-sizing: border-box;
+                -webkit-box-sizing: border-box;
+                text-align: center;
+            }
+            
+            
+                        
+            .prev {
+                display: inline-block;
+                font-size: 14.4px;
+                box-sizing: border-box;
+                -moz-box-sizing: border-box;
+                -webkit-box-sizing: border-box;
+                text-align: center;
+                width: 3%;
+            }
+            
+ .menucabecera ul li:hover{
+                background:rgba(0,0,0,.3);
+            }
+            </style>";
+    }
+
+}
 ?>
+
